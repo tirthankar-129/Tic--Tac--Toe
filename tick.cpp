d5b0ba412db7d60c4733c84326fde9d27e0ace03
tirthankar-129 <112021625+tirthankar-129@users.noreply.github.com>
2023-08-22 13:10:51 +0530

    Update tick.cpp

--- tick.cpp
+++ tick.cpp
@@ -1,179 +1,126 @@
-#include<iostream>
-using namespace std;
+//Tic Tac Toe Game in C++
 
-int num;    //number enter to play.
-char ch[9] = { '1','2','3','4','5','6','7','8','9' }; 
-char player1 = { 'X' };
-char player2 = { 'O' };
-void Draw();
-void player_1();
-void player_2();
-bool X_Wins();
-bool O_Wins();
+//Importing the inbuild libraries in CPP
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+//Array for the board
+char board[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+//Variable Declaration
+int choice;
+int row,column;
+char turn = 'X';
+bool draw = false;
+
+//Function to show the current status of the gaming board
+
+void display_board(){
+
+    //Rander Game Board LAYOUT
+
+    cout<<"PLAYER - 1 [X]\t PLAYER - 2 [O]\n\n";
+    cout<<"\t\t     |     |     \n";
+    cout<<"\t\t  "<<board[0][0]<<"  |  "<<board[0][1]<<"  |  "<<board[0][2]<<" \n";
+    cout<<"\t\t_____|_____|_____\n";
+    cout<<"\t\t     |     |     \n";
+    cout<<"\t\t  "<<board[1][0]<<"  |  "<<board[1][1]<<"  |  "<<board[1][2]<<" \n";
+    cout<<"\t\t_____|_____|_____\n";
+    cout<<"\t\t     |     |     \n";
+    cout<<"\t\t  "<<board[2][0]<<"  |  "<<board[2][1]<<"  |  "<<board[2][2]<<" \n";
+    cout<<"\t\t     |     |     \n";
+}
+
+//Function to get the player input and update the board
+
+void player_turn(){
+    if(turn == 'X'){
+        cout<<"\n\tPlayer - 1 [X] turn : ";
+    }
+    else if(turn == 'O'){
+        cout<<"\n\tPlayer - 2 [O] turn : ";
+    }
+    //Taking input from user
+    //updating the board according to choice and reassigning the turn Start
+
+    cin>> choice;
+
+    //switch case to get which row and column will be update
+
+    switch(choice){
+        case 1: row=0; column=0; break;
+        case 2: row=0; column=1; break;
+        case 3: row=0; column=2; break;
+        case 4: row=1; column=0; break;
+        case 5: row=1; column=1; break;
+        case 6: row=1; column=2; break;
+        case 7: row=2; column=0; break;
+        case 8: row=2; column=1; break;
+        case 9: row=2; column=2; break;
+        default:
+            cout<<"Invalid Move";
+    }
+
+    if(turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O'){
+        //updating the position for 'X' symbol if
+        //it is not already occupied
+        board[row][column] = 'X';
+        turn = 'O';
+    }else if(turn == 'O' && board[row][column] != 'X' && board[row][column] != 'O'){
+        //updating the position for 'O' symbol if
+        //it is not already occupied
+        board[row][column] = 'O';
+        turn = 'X';
+    }else {
+        //if input position already filled
+        cout<<"Box already filled!n Please choose another!!\n\n";
+        player_turn();
+    }
+    /* Ends */
+    display_board();
+}
+
+//Function to get the game status e.g. GAME WON, GAME DRAW GAME IN CONTINUE MODE
+
+bool gameover(){
+    //checking the win for Simple Rows and Simple Column
+    for(int i=0; i<3; i++)
+    if(board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
+    return false;
+
+    //checking the win for both diagonal
+
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
+    return false;
+
+    //Checking the game is in continue mode or not
+    for(int i=0; i<3; i++)
+    for(int j=0; j<3; j++)
+    if(board[i][j] != 'X' && board[i][j] != 'O')
+    return true;
+
+    //Checking the if game already draw
+    draw = true;
+    return false;
+}
+
+//Program Main Method
 
 int main()
 {
-	int n = -1;
-	cout << "\t\t\t\tTic Toc Toe Game\n" << endl;
-	cout << "\t\t\t\tplayer<1>: X\n";
-	cout << "\t\t\t\tplayer<2>: O\n";
-	cout << "\n\n";
-	do
-	{
-		Draw();
-		player_1();
-		Draw();
-		if (X_Wins())
-		{
-			cout << "\t\t\t\tPlayer<1> Wins!" << endl;
-			n = 1;
-			break;
-		}
-		cout << "\n\n\n";
-		player_2();
-		Draw();
-		if (O_Wins())
-		{
-			cout << "\t\t\t\tPlayer<2> Wins!" << endl;
-			n = 1;
-			break;
-		}
-		cout << "\n\n\n";
-	} while (n == -1);
-
+    cout<<"\t\t\tT I C K -- T A C -- T O E -- G A M E\t\t\t";
+    cout<<"\n\t\t\t\tFOR 2 PLAYERS\n\t\t\t";
+    while(gameover()){
+        display_board();
+        player_turn();
+        gameover();
+    }
+    if(turn == 'X' && draw == false){
+        cout<<"\n\nCongratulations!Player with 'X' has won the game";
+    }
+    else if(turn == 'O' && draw == false){
+        cout<<"\n\nCongratulations!Player with 'O' has won the game";
+    }
+    else
+    cout<<"\n\nGAME DRAW!!!\n\n";
 	return 0;
-}
-//Draw
-void Draw()
-{
-	cout <<"\t\t\t\t"<< ch[0] << "  |  " << ch[1] << "  |  " << ch[2] << "  |  " << "\n";
-	cout << "\t\t\t\t----------------" << endl;
-	cout << "\t\t\t\t"<<ch[3] << "  |  " << ch[4] << "  |  " << ch[5] << "  |  " << "\n";
-	cout << "\t\t\t\t----------------" << endl;
-	cout << "\t\t\t\t"<<ch[6] << "  |  " << ch[7] << "  |  " << ch[8] << "  |  " << "\n\n\n";
-}
-//player 1
-void player_1()
-{
-	cout << "\t\t\t\tPlayer<1> Enter number: ";
-	cin >> num;
-	switch (num)
-	{
-	case 1:
-		ch[0] = player1;
-		break;
-	case 2:
-		ch[1] = player1;
-		break;
-	case 3:
-		ch[2] = player1;
-		break;
-	case 4:
-		ch[3] = player1;
-		break;
-	case 5:
-		ch[4] = player1;
-		break;
-	case 6:
-		ch[5] = player1;
-		break;
-	case 7:
-		ch[6] = player1;
-		break;
-	case 8:
-		ch[7] = player1;
-		break;
-	case 9:
-		ch[8] = player1;
-		break;
-	default:
-		cout << "\t\t\t\t\nplease, enter number from <1> to <9>!\n\n" << endl;
-		player_1();
-		break;
-	}
-}
-//player 2
-void player_2()
-{
-	cout << "\t\t\t\tPlayer<2> Enter number: ";
-	cin >> num;
-	switch (num)
-	{
-	case 1:
-		ch[0] = player2;
-		break;
-	case 2:
-		ch[1] = player2;
-		break;
-	case 3:
-		ch[2] = player2;
-		break;
-	case 4:
-		ch[3] = player2;
-		break;
-	case 5:
-		ch[4] = player2;
-		break;
-	case 6:
-		ch[5] = player2;
-		break;
-	case 7:
-		ch[6] = player2;
-		break;
-	case 8:
-		ch[7] = player2;
-		break;
-	case 9:
-		ch[8] = player2;
-		break;
-	default:
-		cout << "\t\t\t\t\nplease, enter number from <1> to <9>!\n\n" << endl;
-		player_2();
-		break;
-	}
-}
-//player 1 wins.
-bool X_Wins()
-{
-	if (ch[0] == 'X' && ch[1] == 'X' && ch[2] == 'X')
-		return true;
-	else if (ch[3] == 'X' && ch[4] == 'X' && ch[5] == 'X')
-		return true;
-	else if (ch[6] == 'X' && ch[7] == 'X' && ch[8] == 'X')
-		return true;
-	else if (ch[0] == 'X' && ch[3] == 'X' && ch[6] == 'X')
-		return true;
-	else if (ch[1] == 'X' && ch[4] == 'X' && ch[7] == 'X')
-		return true;
-	else if (ch[2] == 'X' && ch[5] == 'X' && ch[8] == 'X')
-		return true;
-	else if (ch[0] == 'X' && ch[4] == 'X' && ch[8] == 'X')
-		return true;
-	else if (ch[2] == 'X' && ch[4] == 'X' && ch[6] == 'X')
-		return true;
-	else
-		return false;
-}
-//player 2 wins.
-bool O_Wins()
-{
-	if (ch[0] == 'O' && ch[1] == 'O' && ch[2] == 'O')
-		return true;
-	else if (ch[3] == 'O' && ch[4] == 'O' && ch[5] == 'O')
-		return true;
-	else if (ch[6] == 'O' && ch[7] == 'O' && ch[8] == 'O')
-		return true;
-	else if (ch[0] == 'O' && ch[3] == 'O' && ch[6] == 'O')
-		return true;
-	else if (ch[1] == 'O' && ch[4] == 'O' && ch[7] == 'O')
-		return true;
-	else if (ch[2] == 'O' && ch[5] == 'O' && ch[8] == 'O')
-		return true;
-	else if (ch[0] == 'O' && ch[4] == 'O' && ch[8] == 'O')
-		return true;
-	else if (ch[2] == 'O' && ch[4] == 'O' && ch[6] == 'O')
-		return true;
-	else
-		return false;
-}
-
+} 
